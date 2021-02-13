e0f431af8660a3263ae9efe65f9525c3d1072c97
Kim Yin <kyiimn@gmail.com>
2021-02-13 10:50:54 +0900

    수정

--- js/theme.js
+++ js/theme.js
@@ -1,36 +1,36 @@
 // Store the theme
-let darkTheme = localStorage.getItem("darkTheme");
+let lighttheme = localStorage.getItem("lighttheme");
 const themeToggle = document.querySelector("#themeButton");
 
 // Apply Dark theme
-const enableDark = () => {
-  document.body.classList.add("darktheme");
-  localStorage.setItem("darkTheme", "enabled");
-  themeToggle.innerHTML = `<i id="themeButton__icon" data-feather="sun"></i>`;
-  feather.replace();
-};
-
-// Remove Dark theme
-const disableDark = () => {
-  document.body.classList.remove("darktheme");
-  localStorage.setItem("darkTheme", null);
+const enableLight = () => {
+  document.body.classList.add("lighttheme");
+  localStorage.setItem("lighttheme", "enabled");
   themeToggle.innerHTML = `<i id="themeButton__icon" data-feather="moon"></i>`;
   feather.replace();
 };
 
+// Remove Dark theme
+const disableLight = () => {
+  document.body.classList.remove("lighttheme");
+  localStorage.setItem("lighttheme", null);
+  themeToggle.innerHTML = `<i id="themeButton__icon" data-feather="sun"></i>`;
+  feather.replace();
+};
+
 //Toggle theme
-if (darkTheme === "enabled") {
-  enableDark();
+if (lighttheme === "enabled") {
+  enableLight();
 } else {
-  disableDark();
+  disableLight();
 }
 
 themeToggle.addEventListener("click", () => {
-  darkTheme = localStorage.getItem("darkTheme");
-  if (darkTheme !== "enabled") {
-    enableDark();
+  lighttheme = localStorage.getItem("lighttheme");
+  if (lighttheme !== "enabled") {
+    enableLight();
   } else {
-    disableDark();
+    disableLight();
   }
 });
 
